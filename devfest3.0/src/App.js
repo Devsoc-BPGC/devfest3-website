4b3b2f40a15bf516b52efbc57cbae54010b90273
tanish111 <tanishdesai37@gmail.com>
2023-12-26 11:19:31 +0530

    DevFest3.0 Photo

--- devfest3.0/src/App.js
+++ devfest3.0/src/App.js
@@ -4,20 +4,24 @@ import bgImg from "./StarBg.png";
 import devfest from "./DEVF3ST.png";
 import { motion } from "framer-motion";
 import ParticleAnimation from "./components/ParticleAnimation";
+import FestNo from "./components/FestNo";
 
 function App() {
   return (
-    <Flex direction="column" height={"100vh"} bgImage={bgImg} bgPosition={"center"} bgRepeat={"no-repeat"} bgColor={"black"}>
+    <Flex direction="column" minH={"100%"} bgImage={bgImg} bgPosition={"center"} bgColor={"black"} scrollBehavior={true}>
       <motion.div style={{ opacity: 1 }} animate={{ opacity: 0, transition: { duration: 10 } }}>
         <ParticleAnimation />
       </motion.div>
       <motion.div style={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 0.5, delay: 4 } }}>
         <NavBar />
       </motion.div>
-      <Flex height={"100%"} alignItems={"center"} justifyContent={"center"}>
+      <Flex alignItems={"center"} justifyContent={"center"} height={"100vh"}>
         <motion.div style={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 1, delay: 5 } }}>
           <Image src={devfest} height={150} />
         </motion.div>
+      </Flex>
+      <Flex alignItems={"center"} justifyContent={"center"} height={"100vh"}>
+        <FestNo/>
       </Flex>
     </Flex>
   );
---
+++ devfest3.0/src/components/FestNo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import fest3 from '../3.0Grp.png'
+import { Box } from '@chakra-ui/react'
+function FestNo() {
+  return (
+    <>
+    <Box height={"60%"} width={"60%"} backgroundSize={"contain"} backgroundRepeat={"no-repeat"} backgroundImage={fest3}>
+    </Box>
+    </>
+  )
+}
+
+export default FestNo
