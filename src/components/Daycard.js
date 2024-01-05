f7e9f9014d269f29d4763c1e96850d5f7eecfc44
tanish111 <tanishdesai37@gmail.com>
2024-01-05 22:07:21 +0530

    Minor Fix

--- src/components/Daycard.js
+++ src/components/Daycard.js
@@ -4,14 +4,14 @@ function Daycard(props) {
   return (
     <Box display="flex" flexDir="row" width="100%" marginBottom="1.2rem">
       <Box textAlign="center" width="30%" display="flex" alignItems="center">
-        <Text textColor="white" transform="rotate(180deg)" fontFamily="Megrim" minW={"100%"} fontSize={{ base: "1rem", md: "1.5rem", lg: "2rem", xl: "2.5rem", "2xl": "3rem" }} style={{writingMode:"vertical-rl",textOrientation:"sideways-right"}} >
+        <Text textColor="white" transform="rotate(180deg)" fontFamily="Megrim" minW={"100%"} fontSize={{ base: "1.5rem", md: "2rem", lg: "2.5rem", xl: "3rem", "2xl": "3.5rem" }} style={{writingMode:"vertical-rl",textOrientation:"sideways-right"}} >
           Day-{props.num}
         </Text>
       </Box>
       <Box display="Flex" flexDir="column" height="100%" width="100%" justifyContent="space-between" alignItems="center">
         {props.events.map((item, index) => {
           return (
-            <Box margin="5px" key={index} height={{ base: "3rem", md: "3.75rem", lg: "4.5rem", xl: "5.25rem", "2xl": "6rem" }} width="100%" display="flex" alignItems="center">
+            <Box margin="5px" key={index} height={{ base: "2rem", md: "3.25rem", lg: "4rem", xl: "5rem", "2xl": "6rem" }} width="100%" display="flex" alignItems="center">
               <Text fontFamily="Megrim" textColor="white" fontSize={{ base: "1rem", md: "1.5rem", lg: "2rem", xl: "2.5rem", "2xl": "3rem" }} lineHeight={{ base: "1rem", md: "1.5rem", lg: "2rem", xl: "2.5rem", "2xl": "3rem" }}>
                 <b>{item.name}</b>
               </Text>
