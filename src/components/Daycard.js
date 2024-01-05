6abf9ab2e91cc10a3097b5f63c14402c2f9a5b7c
tanish111 <tanishdesai37@gmail.com>
2024-01-05 21:50:11 +0530

    Text overlapping bug fixed TimeTable

--- src/components/Daycard.js
+++ src/components/Daycard.js
@@ -4,15 +4,15 @@ function Daycard(props) {
   return (
     <Box display="flex" flexDir="row" width="100%" marginBottom="1.2rem">
       <Box textAlign="center" width="30%" display="flex" alignItems="center">
-        <Text textColor="white" transform="rotate(270deg)" fontFamily="Megrim" width={"100%"} fontSize={{ base: "1rem", md: "1.5rem", lg: "2rem", xl: "2.5rem", "2xl": "3rem" }}>
+        <Text textColor="white" transform="rotate(180deg)" fontFamily="Megrim" minW={"100%"} fontSize={{ base: "1rem", md: "1.5rem", lg: "2rem", xl: "2.5rem", "2xl": "3rem" }} style={{writingMode:"vertical-rl",textOrientation:"sideways-right"}} >
           Day-{props.num}
         </Text>
       </Box>
       <Box display="Flex" flexDir="column" height="100%" width="100%" justifyContent="space-between" alignItems="center">
         {props.events.map((item, index) => {
           return (
-            <Box margin="5px" key={index} height={{ base: "2rem", md: "2.5rem", lg: "3rem", xl: "3.5rem", "2xl": "4rem" }} width="90%" display="flex" alignItems="center">
-              <Text fontFamily="Megrim" textColor="white" fontSize={{ base: "1rem", md: "1.5rem", lg: "2rem", xl: "2.5rem", "2xl": "3em" }}>
+            <Box margin="5px" key={index} height={{ base: "3rem", md: "3.75rem", lg: "4.5rem", xl: "5.25rem", "2xl": "6rem" }} width="100%" display="flex" alignItems="center">
+              <Text fontFamily="Megrim" textColor="white" fontSize={{ base: "1rem", md: "1.5rem", lg: "2rem", xl: "2.5rem", "2xl": "3rem" }} lineHeight={{ base: "1rem", md: "1.5rem", lg: "2rem", xl: "2.5rem", "2xl": "3rem" }}>
                 <b>{item.name}</b>
               </Text>
             </Box>
--- src/components/Timecard.js
+++ src/components/Timecard.js
@@ -6,7 +6,7 @@ function Timecard(props) {
       <Box display="Flex" flexDir="column" height="100%" width="100%" justifyContent="space-between" alignItems="center">
         {props.events.map((item, index) => {
           return (
-            <Box margin="5px" key={index} height={{ base: "2rem", md: "2.5rem", lg: "3rem", xl: "3.5rem", "2xl": "4rem" }} width="90%" display="flex" alignItems="center">
+            <Box margin="5px" key={index} height={{ base: "3rem", md: "3.75rem", lg: "4.5rem", xl: "5.25rem", "2xl": "6rem" }} width="90%" display="flex" alignItems="center">
               <Text textColor="white" fontSize={{ base: "0.7rem", md: "1rem", lg: "1.5rem", xl: "2rem", "2xl": "2.5em" }}>
                 {item.time} | {item.venue}
               </Text>
