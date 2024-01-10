542dd3914517d15e5586464ed01f75e71c036646
tanish111 <tanishdesai37@gmail.com>
2024-01-11 00:29:29 +0530

    Sponsor changes

--- src/components/Prizes.jsx
+++ src/components/Prizes.jsx
@@ -1,11 +1,17 @@
-import { Container, Flex, Text } from "@chakra-ui/react";
-
+import { Container, Flex, Text,Image,Link } from "@chakra-ui/react";
+import echo3d from './echo3D.png'
 const WinnerCard = (props) => {
   return (
-    <Container bgColor={"rgba(256,256,256,0.15)"} borderRadius={"16px"} backdropFilter={"blur(1px)"} height={"80%"} width={{ base: "50%", sm: "25%", lg: "15%" }} textAlign={"center"} m={"1%"}>
-      <Text fontFamily={"Epilogue"} fontWeight={800} color={"white"} paddingTop={"5%"}>
+    <Container paddingBottom={"10px"} bgColor={"rgba(256,256,256,0.15)"} borderRadius={"16px"} backdropFilter={"blur(1px)"} width={{ base: "50%", sm: "25%", lg: "15%" }} textAlign={"center"} m={"1%"}>
+      <Text margin={"5px"} fontFamily={"Epilogue"} fontWeight={800} color={"white"} paddingTop={"5%"}>
         {props.title}
-      </Text>
+        </Text>
+        <Image margin={"5px"} src={props.img}></Image>
+        <Text margin={"5px"}color={"white"}>{props.disc}</Text>
+        { (props.formLink!=null) ?
+        <Link fontWeight={"800"} margin={"5px"} color={"#ADD8E6"} href={props.formLink}>Register Here</Link>
+        : <></>
+      }
     </Container>
   );
 };
@@ -18,7 +24,7 @@ const Prizes = () => {
       </Text>
       <Flex flex={2} width={"100%"} direction={"column"}>
         <Flex width={"100%"} height={"100%"} justifyContent={"space-around"} alignItems={"center"} direction={{ base: "column", sm: "row" }}>
-          <WinnerCard title="Winner" />
+          <WinnerCard title="echo3D" img={echo3d} disc={"echo3D a 3D asset management platform that enables developers to manage, update, and stream 3D content to real-time apps and games. For 1-month FREE access to echo3D Pro tier, register here: "} formLink={"https://console.echo3d.com/#/auth/register-promo?code=Jan2024echo925"} />
           <WinnerCard title="First Runner Up" />
           <WinnerCard title="Second Runner Up" />
         </Flex>
