808f73b80d66ee78e0497bfb87c28ed286d5fbc8
Abheshek Murugan <abheshek.murugan@gmail.com>
2024-01-13 16:17:03 +0530

    Added sponsors

--- src/components/Sponsorscard.js
+++ src/components/Sponsorscard.js
@@ -4,6 +4,9 @@ import replit from './replit.png'
 import { Box,Text,Wrap,WrapItem } from '@chakra-ui/react'
 import devfolio from './devfolio.png'
 import outscal from './outscal_logo.jpg'
+import interviewbuddy from './interviewbuddy.png';
+import echo3D from './echo3d2.png';
+
 const Sponsorscard = () => {
   return (
     <Box height="100%" width="100%" display="flex" flexDir="column" alignItems="center">
@@ -24,6 +27,14 @@ const Sponsorscard = () => {
             </WrapItem>
             <WrapItem margin="10px">
             <img src={outscal} alt="OUTSCAL LOGO" />
+            </WrapItem>
+            </Wrap>
+            <Wrap justify={'space-evenly'}>
+            <WrapItem margin="10px" width={'25%'}>
+            <img src={interviewbuddy} alt="INTERVIEWBUDDY LOGO"/>
+            </WrapItem>
+            <WrapItem margin="10px" width={'25%'} alignItems={'center'}>
+            <img src={echo3D} alt="ECHO3D LOGO"/>
             </WrapItem>
             </Wrap>
           </div>
